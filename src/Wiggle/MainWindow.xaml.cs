2df54e35dc15a23334d4e954766a0d11af0429e0
Eric Freeman <eric.freeman@extendhealth.com>
2015-03-04 23:09:08 -0700

    Added in code to actually wiggle mouse.  Now to just get it on a timer.

--- src/Wiggle/MainWindow.xaml.cs
+++ src/Wiggle/MainWindow.xaml.cs
@@ -1,28 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Wiggle
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Wiggle();
+        }
+
+        private void Wiggle()
+        {
+            var point = GetMousePosition();
+            SetCursorPos((int)point.X + 1, (int)point.Y);
+        }
+
+        public static Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
         }
     }
 }
