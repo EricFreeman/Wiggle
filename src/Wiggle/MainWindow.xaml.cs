3535b938bba2b1b772fdb2d957e34ee101153add
Eric Freeman <eric.freeman@extendhealth.com>
2015-03-08 20:35:59 -0600

    Made it minimize to the tray to stay out of your way.

--- src/Wiggle/MainWindow.xaml.cs
+++ src/Wiggle/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Threading;
+using ContextMenu = System.Windows.Forms.ContextMenu;
+using MenuItem = System.Windows.Forms.MenuItem;
 
 namespace Wiggle
 {
@@ -18,19 +23,60 @@ namespace Wiggle
 
         private static Win32Interop.INPUT[] _mouseInput;
 
+        private NotifyIcon _ni = new NotifyIcon();
+
         #endregion
 
-        #region Constructor
+        #region Window
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeNotifyIcon();
 
             _mouseInput = CreateMouseInput();
             _dispatcherTimer.Tick += MoveMouse; 
             _seconds = 10;
             SetInterval(null, null);
             _dispatcherTimer.Start();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            _ni.Visible = false;
+            _ni.Dispose();
+        }
+
+        #endregion
+
+        #region Notify Icon
+
+        private void InitializeNotifyIcon()
+        {
+            _ni.Icon = new System.Drawing.Icon("Icon.ico");
+            _ni.Visible = true;
+            _ni.DoubleClick += delegate { OpenWindow(); };
+            _ni.Text = "Wiggle";
+
+            var cm = new ContextMenu();
+            cm.MenuItems.Add(0, new MenuItem("Open", delegate { OpenWindow(); }));
+            cm.MenuItems.Add(1, new MenuItem("Exit", (sender, args) => Close()));
+            _ni.ContextMenu = cm;
+        }
+
+        private void OpenWindow()
+        {
+            Show();
+            WindowState = WindowState.Normal;
+            Focus();
         }
 
         #endregion
